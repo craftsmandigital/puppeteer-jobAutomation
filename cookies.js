cae8a6a8b6c0e3cfaa1b665a4f3290166388429d
craftsmandigital <hackjack@tutanota.com>
2023-01-05 12:03:48 +0100

    Login to a page and save cookie on browser close. Reuse cookie in another session

---
+++ cookies.js
@@ -0,0 +1,60 @@
+const dotenv = require('dotenv');
+const puppeteer = require("puppeteer");
+const fs = require('fs').promises;
+const sleep = (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
+(async () => {
+  // Load environment variables from the .env file
+  dotenv.config();
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://submit.shutterstock.com/login?language=en", {
+    waitUntil: "networkidle2",
+  });
+
+  // await page.type("#login-username", "jviksaas@yahoo.no");
+  // await page.click("#identifierNext");
+
+  // await page.waitForSelector("#password", {
+  //   visible: true,
+  //   hidden: false,
+  // });
+  // await page.type(
+  //   "#login-password",
+  //   "ypv69HxCA9BhiG"
+  // );
+  // await sleep(1000);
+  // await page.click("#login");
+
+  // await sleep(10000);
+
+  //save cookies
+
+
+
+
+
+  // This function will run when the browser window closes
+  page.on('close', async () => {
+    // You can perform any cleanup tasks here, such as saving data or closing connections
+    //save cookies
+    //const cookies = await page.cookies();
+    await fs.writeFile('./cookies.json', JSON.stringify(cookies, null, 2));
+    console.log('The browser window is closing');
+  });
+
+
+
+  page.on('load', async () => {
+    // You can perform any cleanup tasks here, such as saving data or closing connections
+    //save cookies
+    cookies = await page.cookies();
+    console.log('Siden er fulstendig lastet ned');
+  });
+
+
+
+
+})();
